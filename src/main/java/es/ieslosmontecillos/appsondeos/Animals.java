e99370fc3f81d0f0bcf3c25ed3aa2c3496f3e624
Miguel-Gil <hayudar@hotmail.com>
2023-10-10 14:13:57 +0200

    fix: Classes placeholder fix

--- src/main/java/es/ieslosmontecillos/appsondeos/Animals.java
+++ src/main/java/es/ieslosmontecillos/appsondeos/Animals.java
@@ -1,4 +1,25 @@
 package es.ieslosmontecillos.appsondeos;
 
-public class Animals {
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+
+public class Animals extends GridPane {
+
+    //Creating elements variables
+    Label lblTitle = new Label("Inicio");
+
+    public Animals() {
+        super();
+        //Set aligment
+        this.setAlignment(Pos.TOP_CENTER);
+
+        //Add elements to layout
+        this.setConstraints(lblTitle, 1, 0);
+
+        this.getChildren().add(lblTitle);
+
+
+    }
+
 }
--- src/main/java/es/ieslosmontecillos/appsondeos/Food.java
+++ src/main/java/es/ieslosmontecillos/appsondeos/Food.java
@@ -1,4 +1,25 @@
 package es.ieslosmontecillos.appsondeos;
 
-public class Food {
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+
+public class Food extends GridPane {
+
+    //Creating elements variables
+    Label lblTitle = new Label("Inicio");
+
+    public Food() {
+        super();
+        //Set aligment
+        this.setAlignment(Pos.TOP_CENTER);
+
+        //Add elements to layout
+        this.setConstraints(lblTitle, 1, 0);
+
+        this.getChildren().add(lblTitle);
+
+
+    }
+
 }
