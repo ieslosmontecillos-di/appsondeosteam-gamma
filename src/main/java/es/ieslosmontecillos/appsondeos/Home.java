1473549e6a96b1e81b41171f72b7edcbf3f751f2
Miguel-Gil <hayudar@hotmail.com>
2023-10-11 14:17:37 +0200

    feat: View Class Home

--- src/main/java/es/ieslosmontecillos/appsondeos/Home.java
+++ src/main/java/es/ieslosmontecillos/appsondeos/Home.java
@@ -17,49 +17,55 @@ import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
-public class Home extends GridPane {
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Objects;
+
+public class Home extends VBox {
 
     //Creating elements variables
     Label lblTitle = new Label("Inicio");
     Button btnAbout = new Button("Creditos");
-    //Image imglogo = new Image("assets/images/gamma.jpg");
+
+    FileInputStream imginput = new FileInputStream("src/assets/images/gamma.jpg");
+    Image imglogo = new Image(imginput);
+    ImageView imgViewLogo = new ImageView(imglogo);
+
     Label lblSepu = new Label("Salvador");
     Label lblThiago = new Label("Thiago");
     Label lblAdrian = new Label("Adrian");
     Label lblMiguel = new Label("Miguel Gil Nebro");
 
-    public Home() {
+    public Home() throws FileNotFoundException {
         super();
         //Set aligment
         this.setAlignment(Pos.TOP_CENTER);
 
-        //Style
-        //ImageView imgLogo = new ImageView(imglogo);
+        //Add elements to layout (old, done with GridPane)
 
-        //Add elements to layout
-        this.setConstraints(lblTitle, 1, 0);
+        //this.setConstraints(lblTitle, 1, 0);
         //this.setConstraints(imgLogo, 2, 1);
-        this.setConstraints(btnAbout, 3, 1);
-        this.getChildren().add(lblTitle);
+        //this.setConstraints(btnAbout, 3, 2);
+        //this.getChildren().add(lblTitle);
         //this.getChildren().add(imgLogo);
-        this.getChildren().add(btnAbout);
+        //this.getChildren().add(btnAbout);
+
+        //Add elements to layout (done with Vbox)
+        this.getChildren().addAll(lblTitle,imgViewLogo,btnAbout);
+
 
         //Button Event
-        btnAbout.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                //Creating Stage, VBox and Scene
-                Stage stage = new Stage();
-                stage.initStyle(StageStyle.UTILITY);
-                VBox vbox = new VBox(lblSepu,lblThiago,lblAdrian,lblMiguel);
-                vbox.setAlignment(Pos.CENTER);
-                Scene pagina = new Scene(vbox, 400, 150);
-                //Modality
-                stage.initModality(Modality.APPLICATION_MODAL);
-                stage.setScene(pagina);
-                stage.show();
-            }
-
+        btnAbout.setOnAction(event -> {
+            //Creating Stage, VBox and Scene
+            Stage stage = new Stage();
+            stage.initStyle(StageStyle.UTILITY);
+            VBox vbox = new VBox(lblSepu,lblThiago,lblAdrian,lblMiguel);
+            vbox.setAlignment(Pos.CENTER);
+            Scene pagina = new Scene(vbox, 400, 150);
+            //Modality
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.setScene(pagina);
+            stage.show();
         });
 
     }
