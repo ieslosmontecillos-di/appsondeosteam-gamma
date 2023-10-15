f7d0fe8908059fea418d2d0ea2e364c71d2005fc
Thiago <thiagovignoloferreira@gmail.com>
2023-10-16 00:14:39 +0200

    Feature: Added Travel and Reading views.

--- src/main/java/es/ieslosmontecillos/appsondeos/Reading.java
+++ src/main/java/es/ieslosmontecillos/appsondeos/Reading.java
@@ -1,24 +1,192 @@
 package es.ieslosmontecillos.appsondeos;
 
-import javafx.geometry.Pos;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
-public class Reading extends GridPane {
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
-    //Creating elements variables
-    Label lblTitle = new Label("Inicio");
+public class Reading extends VBox{
 
-    public Reading() {
+    //DECLARACION VARIABLES
+
+    Text readingTittle;
+
+    //PRIMERA PREGUNTA
+    Label lbFirstQue;
+    HBox hbFirstQue;
+    RadioButton rbFirstQueYes;
+    RadioButton rbFirstQueNo;
+    ToggleGroup tgFirstQue;
+
+    //SEGUNDA PREGUNTA
+    Label lbSecondQue;
+    GridPane gpSecondQue;
+    RadioButton rb1SecondQue;
+    RadioButton rb2SecondQue;
+    RadioButton rb3SecondQue;
+    RadioButton rb4SecondQue;
+    ToggleGroup tgSecondQue;
+
+    //TERCERA PREGUNTA
+    Label lbThirQues;
+    HBox hbThirQues;
+    CheckBox cb1ThirQues;
+    CheckBox cb2ThirQues;
+    CheckBox cb3ThirQues;
+    CheckBox cb4ThirQues;
+    CheckBox cb5ThirQues;
+
+    //CUARTA PREGUNTA
+    Label lbFourthQue;
+    GridPane gpFourthQue;
+    RadioButton rb1FourthQue;
+    RadioButton rb2FourthQue;
+    RadioButton rb3FourthQue;
+    RadioButton rb4FourthQue;
+    ToggleGroup tgFourthQue;
+
+    //QUINTA PREGUNTA
+    Label lbFifthQues;
+    Label lbFifthQues2;
+    TextField tfFifthQues;
+    HBox hbFifthQues;
+    RadioButton rbFifthQuesYes;
+    RadioButton rbFifthQuesNo;
+    ToggleGroup tgFifthQues;
+
+    //IMAGEN
+    FileInputStream imginput;
+    Image imglogo;
+    ImageView imgViewLogo;
+
+    public Reading() throws FileNotFoundException {
         super();
-        //Set aligment
-        this.setAlignment(Pos.TOP_CENTER);
+        this.setSpacing(10);
 
-        //Add elements to layout
-        this.setConstraints(lblTitle, 1, 0);
+        //TITULO
+        readingTittle = new Text("Lecturas");
+        readingTittle.setId("title");
 
-        this.getChildren().add(lblTitle);
+        //PRIMERA PREGUNTA
 
+        lbFirstQue = new Label("¿Considera usted que la lectura es importante?");
+
+        hbFirstQue = new HBox(10);
+
+        rbFirstQueYes = new RadioButton("Sí");
+        rbFirstQueNo = new RadioButton("No");
+
+        tgFirstQue = new ToggleGroup();
+
+        rbFirstQueYes.setToggleGroup(tgFirstQue);
+        rbFirstQueNo.setToggleGroup(tgFirstQue);
+
+        hbFirstQue.getChildren().addAll(rbFirstQueYes, rbFirstQueNo);
+
+        this.getChildren().addAll(readingTittle,lbFirstQue, hbFirstQue);
+
+        //SEGUNDA PREGUNTA
+        lbSecondQue = new Label("¿Cuanto tiempo le dedica ha la lectura?");
+
+        gpSecondQue = new GridPane();
+
+        rb1SecondQue = new RadioButton("20 - 30 minutos ");
+        rb2SecondQue = new RadioButton("1 hora");
+        rb3SecondQue = new RadioButton("Mas de 1 hora");
+        rb4SecondQue = new RadioButton("No tengo tiempo");
+
+        tgSecondQue = new ToggleGroup();
+
+        rb1SecondQue.setToggleGroup(tgSecondQue);
+        rb2SecondQue.setToggleGroup(tgSecondQue);
+        rb3SecondQue.setToggleGroup(tgSecondQue);
+        rb4SecondQue.setToggleGroup(tgSecondQue);
+
+        gpSecondQue.add(rb1SecondQue,0,0);
+        gpSecondQue.add(rb2SecondQue,0,1);
+        gpSecondQue.add(rb3SecondQue,1,0);
+        gpSecondQue.add(rb4SecondQue,1,1);
+
+        this.getChildren().addAll(lbSecondQue,gpSecondQue);
+
+        //TERCERA PREGUNTA
+        lbThirQues = new Label("Que genero/s de libro le gusta leer");
+
+        hbThirQues = new HBox(10);
+
+        cb1ThirQues = new CheckBox("Accion");
+        cb2ThirQues = new CheckBox("Romance");
+        cb3ThirQues = new CheckBox("Aventura");
+        cb4ThirQues = new CheckBox("Terror");
+        cb5ThirQues = new CheckBox("Otro");
+
+        hbThirQues.getChildren().addAll(cb1ThirQues,cb2ThirQues,cb3ThirQues,cb4ThirQues,cb5ThirQues);
+
+        this.getChildren().addAll(lbThirQues, hbThirQues);
+
+        //CUARTA PREGUNTA
+        lbFourthQue= new Label("¿Cuantos libros posee en su casa?");
+
+        gpFourthQue = new GridPane();
+
+        rb1FourthQue = new RadioButton("Menos de 5 ");
+        rb2FourthQue = new RadioButton("Entre 5 y 20");
+        rb3FourthQue = new RadioButton("Entre 20 y 50 ");
+        rb4FourthQue = new RadioButton("Mas de 50 ");
+
+        tgFourthQue = new ToggleGroup();
+
+        rb1FourthQue.setToggleGroup(tgFourthQue);
+        rb2FourthQue.setToggleGroup(tgFourthQue);
+        rb3FourthQue.setToggleGroup(tgFourthQue);
+        rb4FourthQue.setToggleGroup(tgFourthQue);
+
+        gpFourthQue.add(rb1FourthQue,0,0);
+        gpFourthQue.add(rb2FourthQue,0,1);
+        gpFourthQue.add(rb3FourthQue,1,0);
+        gpFourthQue.add(rb4FourthQue,1,1);
+
+
+        this.getChildren().addAll(lbFourthQue,gpFourthQue);
+
+        //QUINTA PREGUNTA
+        lbFifthQues = new Label("¿Esta usted leyendo algun libro actualmente?");
+
+        hbFifthQues = new HBox(10);
+
+        rbFifthQuesYes = new RadioButton("Sí");
+        rbFifthQuesNo = new RadioButton("No");
+
+        tgFifthQues = new ToggleGroup();
+
+        rbFifthQuesYes.setToggleGroup(tgFifthQues);
+        rbFifthQuesNo.setToggleGroup(tgFifthQues);
+
+        lbFifthQues2 = new Label("Si la respuesta anterior fue SI escriba aqui su libro");
+
+        tfFifthQues = new TextField();
+        tfFifthQues.setMaxWidth(150);
+
+        hbFifthQues.getChildren().addAll(rbFifthQuesYes, rbFifthQuesNo);
+
+        this.getChildren().addAll(lbFifthQues, hbFifthQues,lbFifthQues2,tfFifthQues);
+
+        //Provisional Button to make the CSV
+        Button btnCheckNmake = new Button("Enviar");
+        this.getChildren().add(btnCheckNmake);
+
+        //IMAGEN
+        imginput = new FileInputStream("src/main/resources/assets/images/books.jpg");
+        imglogo = new Image(imginput);
+        imgViewLogo = new ImageView(imglogo);
+
+        this.getChildren().add(imgViewLogo);
 
     }
 
--- src/main/java/es/ieslosmontecillos/appsondeos/Travel.java
+++ src/main/java/es/ieslosmontecillos/appsondeos/Travel.java
@@ -1,25 +1,196 @@
 package es.ieslosmontecillos.appsondeos;
 
-import javafx.geometry.Pos;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
-public class Travel extends GridPane {
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
-    //Creating elements variables
-    Label lblTitle = new Label("Inicio");
+public class Travel extends VBox {
 
-    public Travel() {
+    //DECLARACION VARIABLES
+    Text travelTittle;
+
+    //PRIMERA PREGUNTA
+    Label lbFirstQue;
+    HBox hbFirstQue;
+    RadioButton rbFirstQueYes;
+    RadioButton rbFirstQueNo;
+    ToggleGroup tgFirstQue;
+
+    //SEGUNDA PREGUNTA
+    Label lbSecondQue;
+    GridPane gpSecondQue;
+    RadioButton rb1SecondQue;
+    RadioButton rb2SecondQue;
+    RadioButton rb3SecondQue;
+    RadioButton rb4SecondQue;
+    RadioButton rb5SecondQue;
+    ToggleGroup tgSecondQue;
+
+    //TERCERA PREGRUNTA
+    Label lbThirdQues;
+    HBox hbThirdQues;
+    RadioButton rb1ThirdQues;
+    RadioButton rb2ThirdQues;
+    RadioButton rb3ThirdQues;
+    RadioButton rb4ThirdQues;
+    RadioButton rb5ThirdQues;
+    ToggleGroup tgThirdQues;
+
+    //CUARTA PREGUNTA
+    Label lbFourthQue;
+    GridPane gpFourthQue;
+    RadioButton rb1FourthQue;
+    RadioButton rb2FourthQue;
+    RadioButton rb3FourthQue;
+    RadioButton rb4FourthQue;
+    ToggleGroup tgFourthQue;
+
+    //QUINTA PREGUNTA
+    Label lbFifthQues;
+    HBox hbFifthQues;
+    CheckBox cb1FifthQues;
+    CheckBox cb2FifthQues;
+    CheckBox cb3FifthQues;
+    CheckBox cb4FifthQues;
+    CheckBox cb5FifthQues;
+
+    //IMAGEN
+    FileInputStream imginput;
+    Image imglogo;
+    ImageView imgViewLogo;
+
+    public Travel() throws FileNotFoundException {
         super();
-        //Set aligment
-        this.setAlignment(Pos.TOP_CENTER);
+        this.setSpacing(10);
 
-        //Add elements to layout
-        this.setConstraints(lblTitle, 1, 0);
+        //TITULO
+        travelTittle = new Text("Viajes");
+        travelTittle.setId("title");
 
-        this.getChildren().add(lblTitle);
+        //PRIMERA PREGUNTA
+        lbFirstQue = new Label("¿Tiene usted pensado viajar proximamente?");
+
+        hbFirstQue = new HBox(10);
+
+        rbFirstQueYes = new RadioButton("Sí");
+        rbFirstQueNo = new RadioButton("No");
+
+        tgFirstQue = new ToggleGroup();
+
+        rbFirstQueYes.setToggleGroup(tgFirstQue);
+        rbFirstQueNo.setToggleGroup(tgFirstQue);
+
+        hbFirstQue.getChildren().addAll(rbFirstQueYes, rbFirstQueNo);
+
+        this.getChildren().addAll(travelTittle,lbFirstQue, hbFirstQue);
+
+        //SEGUNDA PREGUNTA
+        lbSecondQue = new Label("¿Hace cuanto fue su ultimo viaje?");
+
+        gpSecondQue = new GridPane();
+
+        rb1SecondQue = new RadioButton("Hace unos meses ");
+        rb2SecondQue = new RadioButton("1 año");
+        rb3SecondQue = new RadioButton("2 años");
+        rb4SecondQue = new RadioButton("3 años");
+        rb5SecondQue = new RadioButton("Mas de 3 años");
+
+        tgSecondQue = new ToggleGroup();
+
+        rb1SecondQue.setToggleGroup(tgSecondQue);
+        rb2SecondQue.setToggleGroup(tgSecondQue);
+        rb3SecondQue.setToggleGroup(tgSecondQue);
+        rb4SecondQue.setToggleGroup(tgSecondQue);
+        rb5SecondQue.setToggleGroup(tgSecondQue);
+
+        gpSecondQue.add(rb1SecondQue,0,0);
+        gpSecondQue.add(rb2SecondQue,1,0);
+        gpSecondQue.add(rb3SecondQue,2,0);
+        gpSecondQue.add(rb4SecondQue,0,1);
+        gpSecondQue.add(rb5SecondQue,1,1);
+
+        this.getChildren().addAll(lbSecondQue,gpSecondQue);
 
 
+        //TERCERA PREGUNTA
+        lbThirdQues = new Label("¿Cuantos viajes sueles realizar al año?");
+
+        hbThirdQues = new HBox(10);
+
+        rb1ThirdQues = new RadioButton("1");
+        rb2ThirdQues = new RadioButton("2");
+        rb3ThirdQues = new RadioButton("3");
+        rb4ThirdQues = new RadioButton("4 o mas");
+        rb5ThirdQues = new RadioButton("ninguno");
+
+        tgThirdQues = new ToggleGroup();
+
+        rb1ThirdQues.setToggleGroup(tgFirstQue);
+        rb2ThirdQues.setToggleGroup(tgFirstQue);
+        rb3ThirdQues.setToggleGroup(tgFirstQue);
+        rb4ThirdQues.setToggleGroup(tgFirstQue);
+        rb5ThirdQues.setToggleGroup(tgFirstQue);
+
+        hbThirdQues.getChildren().addAll(rb1ThirdQues,rb2ThirdQues,rb3ThirdQues,rb4ThirdQues,rb5ThirdQues);
+
+        this.getChildren().addAll(lbThirdQues,hbThirdQues);
+
+        //CUARTA PREGUNTA
+        lbFourthQue= new Label("¿Cuantos dias sueles estar de viaje?");
+
+        gpFourthQue = new GridPane();
+
+        rb1FourthQue = new RadioButton("Entre 2 y 3 dias ");
+        rb2FourthQue = new RadioButton("Entre 4 y 6 dias ");
+        rb3FourthQue = new RadioButton("Entre 1 y 2 semanas ");
+        rb4FourthQue = new RadioButton("Mas de 2 semanas ");
+
+        tgFourthQue = new ToggleGroup();
+
+        rb1FourthQue.setToggleGroup(tgFourthQue);
+        rb2FourthQue.setToggleGroup(tgFourthQue);
+        rb3FourthQue.setToggleGroup(tgFourthQue);
+        rb4FourthQue.setToggleGroup(tgFourthQue);
+
+        gpFourthQue.add(rb1FourthQue,0,0);
+        gpFourthQue.add(rb2FourthQue,0,1);
+        gpFourthQue.add(rb3FourthQue,1,0);
+        gpFourthQue.add(rb4FourthQue,1,1);
+
+        this.getChildren().addAll(lbFourthQue,gpFourthQue);
+
+        //QUINTA PREGUNTA
+        lbFifthQues = new Label("Que medio/s sueles usar para viajar");
+
+        hbFifthQues = new HBox(10);
+
+        cb1FifthQues = new CheckBox("Coche");
+        cb2FifthQues = new CheckBox("Moto");
+        cb3FifthQues = new CheckBox("Bus");
+        cb4FifthQues = new CheckBox("Barco");
+        cb5FifthQues = new CheckBox("Avion");
+
+        hbFifthQues.getChildren().addAll(cb1FifthQues,cb2FifthQues,cb3FifthQues,cb4FifthQues,cb5FifthQues);
+
+        this.getChildren().addAll(lbFifthQues,hbFifthQues);
+
+        //Provisional Button to make the CSV
+        Button btnCheckNmake = new Button("Enviar");
+        this.getChildren().add(btnCheckNmake);
+
+        //IMAGEN
+        imginput = new FileInputStream("src/main/resources/assets/images/travel.jpg");
+        imglogo = new Image(imginput);
+        imgViewLogo = new ImageView(imglogo);
+
+        this.getChildren().add(imgViewLogo);
     }
 
 }
